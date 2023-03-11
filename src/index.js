bf86fd9c6ef741c1e23d0d772c4e9cf71913dca7
Pawel_Rogowski <pawel.rogowski.praca@gmail.com>
2023-03-11 02:31:30 +0100

    added first component

--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import { Profile } from 'components/Profile';
+import user from 'data/user.json';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
   </React.StrictMode>
 );
