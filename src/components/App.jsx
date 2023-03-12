3bb6f1756ed41d2003a3e9c49e441c83867a576e
Pawel_Rogowski <pawel.rogowski.praca@gmail.com>
2023-03-12 17:19:19 +0100

    added empty component template

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,3 +6,33 @@ import { FriendList } from './FriendList/FriendList';
 import friends from '../data/friends.json';
 import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 import transactions from '../data/transactions.json';
+
+export const App = () => {
+  return (
+    <div
+      style={{
+        padding: 50,
+        height: 'auto',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 100,
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 24,
+        color: '#010101',
+        backgroundColor: 'lightgray',
+      }}
+    >
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" statistics={statistics} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </div>
+  );
+};
