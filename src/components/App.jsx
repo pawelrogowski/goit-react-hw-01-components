8c4fe51d3d8d067ab60aa6d48827865905adde96
Pawel_Rogowski <pawel.rogowski.praca@gmail.com>
2023-03-12 17:16:04 +0100

    Added json and component imports to App.jsx

---
+++ src/components/App.jsx
@@ -0,0 +1,8 @@
+import { Profile } from './Profile/Profile';
+import user from '../data/user.json';
+import { Statistics } from './Statistics/Statistics';
+import statistics from '../data/statistics.json';
+import { FriendList } from './FriendList/FriendList';
+import friends from '../data/friends.json';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import transactions from '../data/transactions.json';
